c75669bdb89f027340d0188951f5e9dbbe883fdf
hassan-46-m <programmer.mohamed@hotmail.com>
2026-02-21 22:14:02 +0200

    +Statefulwidgit

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,20 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+import 'ids.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late List<YoutubePlayerController> controllers;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controllers = ids.map<YoutubePlayerController>((video) {
+      return YoutubePlayerController(
+        initialVideoId: video['id'],
+        flags: const YoutubePlayerFlags(
+          autoPlay: false,
+        ),
+      );
+    }).toList();
+  }
+
+  @override
+  void dispose() {
+    for (var controller in controllers) {
+      controller.dispose();
+    }
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('watch it'),
-        actions:
-        [
-          Icon(Icons.search),
+      appBar: AppBar(backgroundColor: Colors.black,
+        title: const Text('Watch It',style: TextStyle(color: Colors.white),),
+        actions: const [
+          Icon(Icons.search,color: Colors.white,),
           Padding(
-            padding: const EdgeInsets.only(left: 20),
-            child: Icon(Icons.notifications),
-          )],
+            padding: EdgeInsets.only(left: 20),
+            child: Icon(Icons.notifications,color: Colors.white,),
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: ids.length,
+        itemBuilder: (context, index) {
+          return Column(
+            children: [
+              YoutubePlayer(
+                controller: controllers[index],
+                showVideoProgressIndicator: true,
+              ),
+              Text(
+                ids[index]['title'],
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                ),
+              ),
+              const SizedBox(height: 20),
+            ],
+          );
+        },
       ),
     );
   }
